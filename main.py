37febff82035be472b63321dcd4b1bab6e1b5b7c
xs_ismand3ss <2477carlos.a@gmail.com>
2024-09-28 12:47:40 -0600

    add main file

---
+++ main.py
@@ -0,0 +1,51 @@
+import asyncio
+from datetime import datetime
+from bot.bot_data import bot
+from bot import *
+from raspberry.read import read_dht
+from raspberry.handle_data import save_data
+from raspberry.events import check_conditions, generateReports
+
+
+async def raspberry():
+    print("Leyendo sensores...")
+    i = 0
+    h_list = []
+    t_list = []
+    while True:
+        try:
+            h, t = await read_dht()
+            t_list.append(t)
+            h_list.append(h)
+            if i == 40:
+                h_list, t_list, i = await save_data(h_list, t_list, i)
+
+        except Exception as e:
+            print(e)
+            break
+        i += 1
+
+
+async def reports():
+    current_time = datetime.now()
+    await generateReports(current_time)
+    await asyncio.sleep(5)
+    reports()
+
+
+async def main():
+    print("Iniciando procesos...")
+
+    raspberry_task = asyncio.create_task(raspberry())
+    reports_task = asyncio.create_task(reports())
+    await bot.polling()
+
+    asyncio.gather(raspberry_task, reports_task)
+
+
+if __name__ == "__main__":
+    print("Bot inicializado 🤖")
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("\nBot finalizado")
