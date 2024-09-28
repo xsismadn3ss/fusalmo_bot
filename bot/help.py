290e947f4081200685767fab2ce325f50758aff9
xs_ismand3ss <2477carlos.a@gmail.com>
2024-09-28 13:54:22 -0600

    update help command

--- bot/help.py
+++ bot/help.py
@@ -6,16 +6,17 @@ from bot.fx.check_login import login_required
 async def help(message):
     chatid = message.chat.id
     
-    information = """ FUSALMO BOT 🤖🌱🌦️\n
-    Command list\n
-    /status - get current humidity and temperature\n
-    /sign_in - sign in to have all access\n
-    /t_report - get temperature chart from latest data\n
-    /h_report - get humidity chart from latest data\n
-    /reports - get all reports
+    information = """
+\nFUSALMO BOT 🚀🌱🌦️\n
+Command list
+/status - get current humidity and temperature
+/sign_in - sign in to have all access
+/t_report - get temperature chart from latest data
+/h_report - get humidity chart from latest data
+/reports - get all reports
 
-    Information contact:
-    → @xs_ismadn3ss
-    """
+Information contact:
+→ @xs_ismadn3ss
+"""
 
     await bot.send_message(chatid, information)
