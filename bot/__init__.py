babebcebad79813ccd7f6863441e6d3ada796252
xs_ismand3ss <2477carlos.a@gmail.com>
2024-09-28 13:29:37 -0600

    add help command

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,6 +1,9 @@
 print("Cargando comandos 📀")
 
 from bot.start import welcome
-from bot.reports import reports
+from bot.reports import reports, h_reports, t_reports
 from bot.sign_in import sign_in
 from bot.status import status
+from bot.help import help
+
+print("Commando cargados exitosamente 🎉")
---
+++ bot/help.py
@@ -0,0 +1,19 @@
+from bot.bot_data import bot
+from bot.fx.check_login import login_required
+
+@bot.message_handler(commnads=["help"])
+@login_required
+async def help(message):
+    chatid = message.chat.id
+    
+    information = """ FUSALMO BOT\n
+    Command list\n
+    /status - get current humidity and temperature\n
+    /sign_in - sign in to have all access\n
+    /t_report - get temperature chart from latest data\n
+    /h_report - get humidity chart from latest data\n
+    /reports - get all reports
+
+    Information contact:
+    → @xs_ismadn3ss
+    """
